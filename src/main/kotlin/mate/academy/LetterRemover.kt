f1627c13453fed7e9dce7ad1059391d800acce99
Alona <alenavyrodova@gmail.com>
2024-04-02 14:23:38 +0300

    work with companion object

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,13 @@
 package mate.academy
 
-class LetterRemover {
-    // implement ...
+object LetterRemover {
+    @JvmStatic
+    fun removeLetter(input: String): CharArray {
+        val stringToArray = CharArray(input.length)
+        val inputLower = input.lowercase()
+        for ((index, char) in inputLower.withIndex()) {
+            stringToArray[index] = if (char != 'r') char else '!'
+        }
+        return stringToArray
+    }
 }
