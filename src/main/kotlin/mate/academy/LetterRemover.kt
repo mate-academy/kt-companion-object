632f4d1318170ce2c7286c29e98c4709ed219fb8
khorkovenko <khorkovenko>
2024-04-08 10:24:15 +0300

    fix companion object, but detekt build failure

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,9 +1,19 @@
 package mate.academy
 
-object LetterRemover {
-    @JvmStatic
-    fun removeLetter(input: String): CharArray {
-        val replacedString = input.lowercase().replace('r', '!')
-        return replacedString.toCharArray()
+//object LetterRemover {
+//    @JvmStatic
+//    fun removeLetter(input: String): CharArray {
+//        val replacedString = input.lowercase().replace('r', '!')
+//        return replacedString.toCharArray()
+//    }
+//}
+
+class LetterRemover {
+    companion object {
+        @JvmStatic
+        fun removeLetter(input: String): CharArray {
+            val replacedString = input.lowercase().replace('r', '!')
+            return replacedString.toCharArray()
+        }
     }
 }
