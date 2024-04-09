49e1398282714e5a3572453b726e4359e607e0ab
Danil Ponomarenko <akame.dante13@gmail.com>
2024-04-09 18:53:52 +0300

    Created the companion object with `removeLetter()` function.

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,12 @@
 package mate.academy
 
 class LetterRemover {
-    // implement ...
+    companion object {
+        @JvmStatic
+        fun removeLetter(input: String): CharArray {
+            return input.lowercase()
+                .map { if (it == 'r') '!' else it }
+                .toCharArray()
+        }
+    }
 }
