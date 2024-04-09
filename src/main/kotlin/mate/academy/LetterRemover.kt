23217b232683f96f2c66da904a3c007f736911b5
Danil Ponomarenko <akame.dante13@gmail.com>
2024-04-09 19:10:13 +0300

    Replaced LetterRemover class into object to pass build check

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,12 +1,10 @@
 package mate.academy
 
-class LetterRemover {
-    companion object {
-        @JvmStatic
-        fun removeLetter(input: String): CharArray {
-            return input.lowercase()
-                .map { if (it == 'r') '!' else it }
-                .toCharArray()
-        }
+object LetterRemover {
+    @JvmStatic
+    fun removeLetter(input: String): CharArray {
+        return input.lowercase()
+            .map { if (it == 'r') '!' else it }
+            .toCharArray()
     }
 }
