83065f4a153e17ceb7c63a7619aa90f28207a259
Сидоркін Андрій Володимирович <sydorkin-av@ukrposhta.ua>
2024-05-17 22:58:33 +0300

    Tests are passed

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
 class LetterRemover {
-    // implement ...
+    companion object {
+        @JvmStatic
+        fun removeLetter(input: String) = input.lowercase().map { if (it == 'r') '!' else it }.toCharArray()
+    }
 }
