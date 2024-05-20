b571a9f61d643f3988c0aa70b5ee7264081e2465
Сидоркін Андрій Володимирович <sydorkin-av@ukrposhta.ua>
2024-05-20 16:39:39 +0300

    Corrected acc to code review

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -3,6 +3,6 @@ package mate.academy
 class LetterRemover private constructor() {
     companion object {
         @JvmStatic
-        fun removeLetter(input: String) = input.lowercase().map { if (it == 'r') '!' else it }.toCharArray()
+        fun removeLetter(input: String) = input.lowercase().replace('r', '!').toCharArray()
     }
 }
