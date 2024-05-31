b778e852b729d721af36bfe2769272683c420d56
Nonzeq <bgdn.kobylchak@gmail.com>
2024-05-31 15:08:38 +0300

    implemented

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,15 @@
 package mate.academy
 
 class LetterRemover {
-    // implement ...
+    companion object {
+        private const val REPLACED_WITH: Char = '!'
+        private val charsToReplace: Set<Char> = setOf('R', 'r')
+
+        @JvmStatic
+        fun removeLetter(input: String): CharArray {
+            val result: CharArray = input.lowercase().toCharArray()
+            input.forEachIndexed { idx, item -> if (item in charsToReplace) result[idx] = REPLACED_WITH }
+            return result
+        }
+    }
 }
