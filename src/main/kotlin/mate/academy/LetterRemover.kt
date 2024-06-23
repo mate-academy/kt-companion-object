aa1ac6deface90961baa7eb185bc462ba3dd6e5b
reamfox <sashafarion@gmail.com>
2024-06-23 20:59:26 +0300

    add utility class LetterRemover

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,14 @@
 package mate.academy
 
-class LetterRemover {
-    // implement ...
+class LetterRemover private constructor() {
+    companion object {
+        private const val TARGET_CHAR = 'r'
+        private const val REPLACEMENT_CHAR = '!'
+        @JvmStatic fun removeLetter(input: String): CharArray {
+            return input
+                .lowercase()
+                .map { if (it == TARGET_CHAR) REPLACEMENT_CHAR else it }
+                .toCharArray()
+        }
+    }
 }
