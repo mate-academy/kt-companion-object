d5e46493f39a7975df2eed1febd8ad04ed21aec3
Petr <smp22pl@gmail.com>
2024-06-27 13:20:01 +0200

    Companion object method to remove all 'r' characters from a string, replacing each with '!' and returning an array of lowercase characters

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,14 @@
 package mate.academy
 
 class LetterRemover {
-    // implement ...
+    companion object {
+        fun removeLetter(line : String) : CharArray {
+            return line.lowercase().replace("r", "!").toCharArray()
+        }
+    }
+}
+
+fun main() {
+    val input = "Remove Letters"
+    println(LetterRemover.removeLetter(input).toList())
 }
