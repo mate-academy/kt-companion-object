da7be32d712a20834e31c1dabc8a693b75c65c94
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-07-06 16:28:51 +0300

    added hw solution

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,10 @@
 package mate.academy
 
-class LetterRemover {
-    // implement ...
+const val SYMBOL_R_LOWERCASE = 'r'
+const val REPLACING_SYMBOL = '!'
+
+object LetterRemover {
+    @JvmStatic
+    fun removeLetter(input: String) =
+        input.lowercase().replace(SYMBOL_R_LOWERCASE, REPLACING_SYMBOL).toCharArray()
 }
