1faf47aa784c98f30524553a74ab58df2baca18d
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-07-19 04:15:38 +0300

    refactor [kotlin]: simplify impl

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -3,9 +3,9 @@ package mate.academy
 class LetterRemover private constructor() {
 
     companion object {
-        @JvmStatic fun removeLetter(input: String): CharArray = input
-            .map { if (it.lowercaseChar() == 'r') '!' else it.lowercaseChar() }
-            .toCharArray()
+        @JvmStatic
+        fun removeLetter(input: String): CharArray =
+            input.replace("r", "!", ignoreCase = true).lowercase().toCharArray()
     }
 
 }
