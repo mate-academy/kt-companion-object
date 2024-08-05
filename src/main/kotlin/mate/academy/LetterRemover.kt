fcaf21ac3fcb64810611a85b030938150c75310f
TonyH277 <gajjduktoxa@gmail.com>
2024-08-05 21:19:52 +0300

    update LetterRemover

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -4,10 +4,7 @@ class LetterRemover private constructor() {
     companion object {
         @JvmStatic
         fun removeLetter(string: String): CharArray {
-            return string.lowercase().asSequence()
-                .map { if (it == 'r') '!' else it }
-                .toList()
-                .toCharArray()
+            return string.replace('r', '!', ignoreCase = true).toCharArray()
         }
     }
 }
