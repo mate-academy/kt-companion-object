9026f138deeaf04733cfecf4d54a4cf4572662a3
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2024-08-18 07:00:06 +0300

    added code

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,10 +1,8 @@
 package mate.academy
 
-class LetterRemover {
-    companion object {
-        fun removeLetter(input: String): CharArray {
-            val result = input.lowercase().replace('r', '!')
-            return result.toCharArray()
-        }
+object LetterRemover {
+    fun removeLetter(input: String): CharArray {
+        val result = input.lowercase().replace('r', '!')
+        return result.toCharArray()
     }
 }
