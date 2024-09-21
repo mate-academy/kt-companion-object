75ce1db1a047144f1c78a004e68918eed59b9d81
Serhii Nazarenko <nasarenko82@gmail.com>
2024-09-20 22:04:14 -0600

    the first commit in the project

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,13 @@
 package mate.academy
 
-class LetterRemover {
-    // implement ...
+object LetterRemover {
+    @JvmStatic fun removeLetter(input: String): CharArray {
+        val arr = CharArray(input.length)
+        for (i in input.indices) {
+            if (input.lowercase()[i] == 'r') {
+                arr[i] = '!'
+            } else arr[i] = input.lowercase()[i]
+        }
+        return arr
+    }
 }
