1c35d03a4db3d29881ff5541fa9f3276a2f277ce
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-09-30 19:21:55 +0300

    Implemented the feature

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,16 @@
 package mate.academy
 
 class LetterRemover {
-    // implement ...
+    companion object {
+        @JvmStatic
+        fun removeLetter(input: String): CharArray {
+            val characters : MutableList<Char> = mutableListOf()
+            val finalInput = input.lowercase()
+            for (character in finalInput) {
+                if (character == 'r') characters.add('!') else characters.add(character)
+            }
+
+            return characters.toCharArray()
+        }
+    }
 }
