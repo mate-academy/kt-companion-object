bf6ce7b4229b0bf887bf5223206cbadac12f72af
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-10-01 17:53:23 +0300

    Refactored the code

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -3,12 +3,6 @@ package mate.academy
 object LetterRemover {
         @JvmStatic
         fun removeLetter(input: String): CharArray {
-            val characters : MutableList<Char> = mutableListOf()
-            val finalInput = input.lowercase()
-            for (character in finalInput) {
-                if (character == 'r') characters.add('!') else characters.add(character)
-            }
-
-            return characters.toCharArray()
+            return input.replace("r", "!", true).lowercase().toCharArray()
         }
 }
