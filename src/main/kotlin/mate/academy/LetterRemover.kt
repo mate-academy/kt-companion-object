d8d8921188079164885264534efa445a6c928118
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-10-22 10:56:59 +0300

    LetterRemover.kt

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -4,7 +4,7 @@ object LetterRemover {
     @JvmStatic
     fun removeLetter(input: String): CharArray {
         return input.lowercase()
-                    .map { if (it == 'r') '!' else it }
+                    .replace('r', '!')
                     .toCharArray()
     }
 }
