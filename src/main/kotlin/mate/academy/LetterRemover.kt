b476c0534001cbb474ca9cb21baa7efdb7683574
Anatolii Nazarov <anatoly_n@ukr.net>
2024-12-05 16:32:24 +0200

    For check

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,12 +1,9 @@
 package mate.academy
 
-class LetterRemover {
-    // implement ...
-    companion object {
-        @JvmStatic
-        fun removeLetter(str: String): CharArray {
-            val modifiedString = str.lowercase().replace("r", "!")
-            return modifiedString.toCharArray() // Converts the modified string to a CharArray
-        }
+object LetterRemover {
+    @JvmStatic
+    fun removeLetter(str: String): CharArray {
+        val modifiedString = str.lowercase().replace("r", "!")
+        return modifiedString.toCharArray()
     }
 }
