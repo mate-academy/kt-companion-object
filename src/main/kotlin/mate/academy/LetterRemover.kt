22bfeed2e048c7d4893f7a77b0b49dcf834ef6f2
IvanovYurii <ivanovyura76@gmail.com>
2024-12-23 23:55:41 +0200

    Solution of this task

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,22 @@
 package mate.academy
 
-class LetterRemover {
-    // implement ...
+import java.util.*
+
+object LetterRemover {
+    // Статичний метод, доступний в Java
+    @JvmStatic
+    fun removeLetter(input: String): CharArray {
+        val lowercasedInput = input.lowercase(Locale.getDefault())
+        val result = StringBuilder()
+
+        for (c in lowercasedInput) {
+            if (c == 'r') {
+                result.append('!')
+            } else {
+                result.append(c)
+            }
+        }
+
+        return result.toString().toCharArray()
+    }
 }
