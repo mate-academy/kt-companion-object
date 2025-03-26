6eaf83ac8f0564b0ef347226d7f4c4e6eab6270d
aksoli666 <olexsandradorofeieva@gmail.com>
2025-03-26 11:44:23 +0100

    1.0 Done Forbidden letter

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,10 @@
 package mate.academy
 
-class LetterRemover {
-    // implement ...
+const val REPLACE = "r"
+const val REPLACE_WITH = "!"
+
+object LetterRemover {
+    @JvmStatic
+    fun removeLetter(input: String) =
+        input.lowercase().replace(REPLACE, REPLACE_WITH).toCharArray()
 }
