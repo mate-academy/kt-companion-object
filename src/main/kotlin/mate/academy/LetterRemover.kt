6b6e2250b52b3771f4e9798e00dfbf9fd6eca54d
Serhii Shyian <serhii.shn@gmail.com>
2025-03-26 16:16:00 +0200

    added forbidden-letter solution

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -2,4 +2,15 @@ package mate.academy
 
 class LetterRemover {
     // implement ...
+    companion object {
+        @JvmStatic
+        fun removeLetter(input: String): CharArray {
+            return input
+                .lowercase()
+                .map {
+                    if (it == 'r') '!' else it
+                }
+                .toCharArray()
+        }
+    }
 }
