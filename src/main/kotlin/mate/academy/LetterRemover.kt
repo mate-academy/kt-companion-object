d4b5ec16e2f2dc96ba15ab176bb12aae04af488b
Dima Davydov <Kilo6559@gmail.com>
2025-05-22 13:11:40 +0300

    Success

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,18 @@
 package mate.academy
 
-class LetterRemover {
-    // implement ...
+object LetterRemover {
+        @JvmStatic
+        fun removeLetter(input: String): CharArray {
+            val result = mutableListOf<Char>()
+            val lowercase = input.lowercase()
+            for (char in lowercase) {
+                if (char == 'r') {
+                    result.add('!')
+                } else {
+                    result.add(char)
+                }
+            }
+            return result.toCharArray()
+        }
 }
+
